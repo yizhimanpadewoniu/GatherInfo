4939598413975f5195de757a249991d619c75def
yizhimanpadewoniu <yizhimanpadewoniu@yeah.net>
2018-03-06 15:39:28 +0800

    修改cmdline.pu

--- lib/cmdline.py
+++ lib/cmdline.py
@@ -9,16 +9,17 @@ import re
 def parse_args():
     parser = argparse.ArgumentParser(prog="GatherInfo", formatter_class=argparse.RawTextHelpFormatter,
                                      description="A Tiny Toy For Gather Info.\nBy Am4zing(www.*********)",
-                                     usage="Python gather.py [options]")
-    parser.add_argument('-d', '--domain', help='The Target Url. eg: google.com', type=str, required=True)
-    parser.add_argument('-host', '--host', help='The Remote SSH Host IP.', type=str, required=True)
-    parser.add_argument('-p', '--password', help='The Remote SSH Host Password.', type=str, required=True)
-    parser.add_argument('-u', '--username', help='The Remote SSH Host Username.', type=str, required=True)
+                                     usage="Python GatherInfo.py [options]")
+    parser.add_argument('-d', '--domain', help='The Target Url. eg: google.com', type=str, )
+    parser.add_argument('-host', '--host', help='The Remote SSH Host IP.', type=str, )
+    parser.add_argument('-p', '--password', help='The Remote SSH Host Password.', type=str, )
+    parser.add_argument('-u', '--username', help='The Remote SSH Host Username.', type=str, )
     parser.add_argument('-a', '--auto', help='Running Auto Mode.', action='store_true')
     parser.add_argument('-in', '--intercative', help='Running Interactive Mode.', action='store_true')
-    parser.add_argument('-port', '--port', help='The Remote SSH Host Port.', type=int, required=True)
-    # parser.add_argument('-p', help='The Remote SSH Host Password.', type=str)
-    # parser.add_argument('-p', help='The Remote SSH Host Password.', type=str)
+    parser.add_argument('-port', '--port', help='The Remote SSH Host Port.', type=int, )
+    parser.add_argument('-l', '--local-mode', help='Run this script at local machine.', type=str)
+    parser.add_argument('-r', '--remote-mode', help='Use the Remote SSH Host.',
+                        usage='Python GatherInfo.py -r -host 1.1.1.1 -u user -p password -port 22 -d xxx.com', type=str)
     # parser.add_argument('-p', help='The Remote SSH Host Password.', type=str)
 
     if len(sys.argv) == 1:
@@ -31,13 +32,13 @@ def parse_args():
 
 
 def check_args(args):
-    # if not args.i and not args.u and not args.p:
-    #     msg1 = '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ \n' \
-    #           'Args Missing! One of the following args should be specified \n' \
-    #           '-i 192.168.1.1 \n' \
-    #           '-u root \n' \
-    #           '-p password'
-    #     raise Exception(msg1)
+    if not args.l or not args.r:
+        msg1 = '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ \n' \
+              'Args Missing! One of the following args should be specified \n' \
+              '-i 192.168.1.1 \n' \
+              '-u root \n' \
+              '-p password'
+        raise Exception(msg1)
     if type(args.port) != int:
         msg2 = '++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ \n' \
               'The type of port is int.'
